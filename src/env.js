2a2952f77f9c098bfb846f8a85a5921de3625ac9
sepvs <156862630+sepvs@users.noreply.github.com>
2025-05-07 07:39:21 -0500

    Commit Actualización funcionalidad

--- src/env.js
+++ src/env.js
@@ -16,7 +16,10 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
-      .default("development"),
+      .default("development"),     
+    CLOUDINARY_CLOUD_NAME: z.string().min(1), 
+    CLOUDINARY_API_KEY: z.string().min(1),    
+    CLOUDINARY_API_SECRET: z.string().min(1),
   },
 
   /**
@@ -25,7 +28,9 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_ROBOFLOW_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_ROBOFLOW_MODEL_ID: z.string().min(1),
+    NEXT_PUBLIC_ROBOFLOW_VERSION: z.string().min(1),
   },
 
   /**
@@ -38,6 +43,12 @@ export const env = createEnv({
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_ROBOFLOW_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_ROBOFLOW_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_ROBOFLOW_MODEL_ID: process.env.NEXT_PUBLIC_ROBOFLOW_MODEL_ID,
+    NEXT_PUBLIC_ROBOFLOW_VERSION: process.env.NEXT_PUBLIC_ROBOFLOW_VERSION,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
